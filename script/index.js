cb13a86059d951b242aa95c1609053f44bb658e1
Rakibul-Hasan270 <rakibx270@gmail.com>
2024-11-14 19:32:08 +0600

    added mark as read, title, view count etc

--- script/index.js
+++ script/index.js
@@ -1,4 +1,5 @@
 const loadData = async () => {
+    spinner(true);
     const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
     const info = await res.json();
     const data = info.posts;
@@ -10,10 +11,11 @@ const displayDiscuss = (data) => {
     const discussContainer = document.getElementById('discuss-container');
 
     data.forEach(element => {
-        console.log(element);
+        // console.log(element);
 
         // create div 
         const div = document.createElement('div');
+        div.classList.add('mb-2');
 
         // set innerHtml 
         div.innerHTML = `
@@ -21,7 +23,7 @@ const displayDiscuss = (data) => {
         <div class="md:flex bg-slate-50 rounded-2xl p-12">
                         <div class="mr-1 flex justify-center mx-auto">
                             <img class="w-[70px] h-[70px]" src="${element.image}" alt="">
-                            <div class="relative h-3 w-3 bg-green-500 rounded-full right-2 bottom-1">
+                            <div id="active" class="relative h-3 w-3 rounded-full right-2 bottom-1">
                             </div>
                         </div>
                         <div class="">
@@ -30,13 +32,12 @@ const displayDiscuss = (data) => {
                                 <p>Author: <span>${element.author.name}</span></p>
                             </div>
                             <h4 class="text-2xl mb-4">${element.title}</h4>
-                            <p class="border-b border-dotted pb-3 mb-4">${element.description}</p>
+                            <p class="border-b-2 divide-dashed pb-3 mb-4 md:w-[650px]">${element.description}</p>
                             <div class="flex justify-between">
                                 <div class="flex gap-4">
                                     <div>
                                         <i class="fa-regular fa-message"></i>
-                                        <span>${element.
-                                            comment_count}</span>
+                                        <span>${element.comment_count}</span>
                                     </div>
                                     <div>
                                         <i class="fa-solid fa-eye"></i>
@@ -47,16 +48,49 @@ const displayDiscuss = (data) => {
                                         <span>${element.posted_time}</span>
                                     </div>
                                 </div>
-                                <button class="bg-green-600 px-3 py-1 rounded-xl"><i class="fa-solid fa-envelope"></i>
+                                <button onclick="markAsRead('${element.title}','${element.view_count}')" class="bg-green-600 px-3 py-1 rounded-xl"><i class="fa-solid fa-envelope"></i>
                                 </button>
                             </div>
                         </div>
                     </div>
 
         `;
+
         // appendChild 
         discussContainer.appendChild(div);
+        spinner(false);
     });
+}
+
+// spinner/loader 
+const spinner = (isLoading) => {
+    const loader = document.getElementById('loading');
+    if (isLoading) {
+        loader.classList.remove('hidden');
+    }
+    else {
+        loader.classList.add('hidden');
+    }
+}
+
+// mark as read 
+const markAsRead = (title, view_count) => {
+    // read mark count 
+    const readMark = document.getElementById('read-mark');
+    readMark.innerText++;
+
+    // added title or view_count 
+    const markReadContainer = document.getElementById('mark-read');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    
+        <div class="flex justify-between bg-slate-200 rounded-lg p-3 mt-4">
+            <h3 class="text-xl md:w-4/5">${title}</h3>
+            <p><i class="fa-regular fa-eye"></i> <span>${view_count}</span> </p>
+        </div>
+
+    `;
+    markReadContainer.appendChild(div);
 }
 
 loadData();
